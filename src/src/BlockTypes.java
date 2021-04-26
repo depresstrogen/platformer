198efb052c60a930556050276805926af8ce05c1
rllyp <rllyp@users.noreply.github.com>
2021-04-25 22:37:36 -0400

    Drawing Optimized, Loads in images at the start instead of every frame..

---
+++ src/src/BlockTypes.java
@@ -0,0 +1,9 @@
+package src;
+
+public class BlockTypes {
+	public static final String[] blockList = {"ground", "dirt", "brick", "grid", "lavatop", "lavabase"};
+	
+	public String[] getBlockList() {
+		return blockList;
+	}
+}
